b62ea4f1ca331bbbf376080f5710a0476d999089
Saara Khan <khansarah0716@gmail.com>
2024-12-21 17:43:07 +0530

    www

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 // Routes
 app.use('/', require('./routes/index'));
 
